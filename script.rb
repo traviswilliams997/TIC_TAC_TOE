70e7f1ce377bd6eed1f53564570b923447247308
Travis Williams <traviswilliams997@gmail.com>
2022-07-03 06:18:00 -0500

    Add logic to test current code

--- script.rb
+++ script.rb
@@ -12,7 +12,7 @@ end
 
 #Holds functions and state of plaer positions
 class Board
-    
+    attr_accessor :@a1;
   def initialize
       @a1 = ' ';
       @a2 = ' ';
@@ -25,3 +25,10 @@ class Board
       @c3 = ' ';
   end
 end
+
+
+board = Board.new
+p1 = Player1.new
+p @a1
+p1.player_choice("X", @a1)
+p @a1
