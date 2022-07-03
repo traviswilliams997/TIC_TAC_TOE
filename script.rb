f9f31ae6f56387d894944a6ebffba88ffea25df4
Travis Williams <traviswilliams997@gmail.com>
2022-07-03 06:02:35 -0500

    Create class for board

---
+++ script.rb
@@ -0,0 +1,14 @@
+#Holds functions and state of plaer positions
+class Board
+  def initialize
+      let a1 = ' ';
+      let a2 = ' ';
+      let a3 = ' ';
+      let b1 = ' ';
+      let b2 = ' ';
+      let b3 = ' ';
+      let c1 = ' ';
+      let c2 = ' ';
+      let c3 = ' ';
+  end
+end
