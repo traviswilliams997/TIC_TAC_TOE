4ee4e0befe56a67358309364688afcc0cd3dfa04
Travis Williams <traviswilliams997@gmail.com>
2022-07-04 11:02:02 -0500

    feat: Add way for player to change board

--- script.rb
+++ script.rb
@@ -1,34 +1,57 @@
-class Player1
+class Board 
+
   def initialize
-    @score = 0;
-
+    $a1 = 'a';
+    $a2 = 'b';
+    $a3 = 'c';
+    $b1 = ' ';
+    $b2 = ' ';
+    $b3 = ' ';
+    $c1 = ' ';
+    $c2 = ' ';
+    $c3 = ' ';
   end
-  def player_choice (choice, position)
-   position = choice;
-  end
-
 end
 
+class Player1 
 
-#Holds functions and state of plaer positions
-class Board
-    attr_accessor :@a1;
-  def initialize
-      @a1 = ' ';
-      @a2 = ' ';
-      @a3 = ' ';
-      @b1 = ' ';
-      @b2 = ' ';
-      @b3 = ' ';
-      @c1 = ' ';
-      @c2 = ' ';
-      @c3 = ' ';
+  def initialize()
+    @score = 0;
   end
+    
+  def player_choice(choice, position)
+    if position == "a1"
+      $a1 = choice;
+    end
+    if position == "a2"
+      $a2 = choice;
+    end
+    if position == "a3"
+      $a3 = choice;
+    end
+    if position == "b1"
+      $b1 = choice;
+    end
+    if position == "b2"
+      $b2 = choice;
+    end
+    if position == "b3"
+      $b3 = choice;
+    end
+    if position == "c1"
+      $c1 = choice;
+    end
+    if position == "c2"
+      $c2 = choice;
+    end
+    if position == "c3"
+      $c3 = choice;
+    end   
+  end  
 end
 
 
 board = Board.new
-p1 = Player1.new
-p @a1
-p1.player_choice("X", @a1)
-p @a1
+p1 = Player1.new()
+p1.player_choice("X", "c3")
+
