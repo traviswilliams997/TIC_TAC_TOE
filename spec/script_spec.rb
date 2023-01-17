3c932ed7a76708cba6ed1027905d346677fa497d
Travis Williams <traviswilliams997@gmail.com>
2023-01-17 18:53:34 -0500

    Create add spec helper file

---
+++ spec/script_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require_relative '../script'
+
+RSPEC.describe Game do
+    # describe '#initialize' do
+    #   xit 'Shows Board Initial Board State' do
+      
+    #   end
+    # end
+
+end
